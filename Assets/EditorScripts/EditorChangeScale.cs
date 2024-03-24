e3b461e2af098ea1915e4f912e68a578d3d26cbc
Qiu_Mi <beafraid@163.com>
2024-03-24 18:06:10 +0800

    - Update to SDK 0.5.0. -Update gitignore

--- Assets/EditorScripts/EditorChangeScale.cs
+++ Assets/EditorScripts/EditorChangeScale.cs
@@ -1,8 +1,11 @@
+#if UNITY_EDITOR
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class TestScalePos : MonoBehaviour
+/// <summary>
+/// 将模型进行整体移动，以便适配其缩放值
+/// </summary>
+public class EditorChangeScale : MonoBehaviour
 {
 
     public float scale = 2.5f;
@@ -15,3 +18,4 @@ public class TestScalePos : MonoBehaviour
         }
     }
 }
+#endif
